268f308dc2594d1714366bfea7de30c12483177c
aya2372002 <125696175+aya2372002@users.noreply.github.com>
2024-05-19 16:19:02 +0300

    changepassword

--- admin/changePassword.php
+++ admin/changePassword.php
@@ -29,3 +29,28 @@ if (isset($_POST['submit'])) {
     if (count($errors) === 0) {
          // hash the new password
          $newpassword = password_hash($newpassword, PASSWORD_DEFAULT);
+
+         // update password query
+         $SQL = "
+             UPDATE student SET password=:newpassword WHERE indexnumber=:indexnumber;
+             UPDATE authentication SET password=:newpassword WHERE indexnumber=:indexnumber;
+         ";
+ 
+         $stmt = $conn->prepare($SQL);
+         $stmt->bindParam('s', $newpassword, PDO::PARAM_STR);
+         $stmt->bindParam('s', $indexnumber, PDO::PARAM_STR);
+ 
+         $isExecuted = $stmt->execute([
+             'indexnumber' => $indexnumber,
+             'newpassword' => $newpassword
+         ]);
+ 
+         if ($isExecuted) {
+             $_SESSION['message'] = $indexnumber . ", Your Password Has Been Updated";
+             $_SESSION['alert-class'] = "alert alert-success";
+         } else {
+             $_SESSION['message'] = $indexnumber . ", Your Password Failed To Be Updated";
+             $_SESSION['alert-class'] = "alert alert-success";
+         } 
+    }
+}
