54853d96ae25b34587261c23d62f7eca581da4de
aya2372002 <125696175+aya2372002@users.noreply.github.com>
2024-05-19 16:29:32 +0300

    release

--- admin/releaseSpace.php
+++ admin/releaseSpace.php
@@ -21,3 +21,35 @@ if ($userCount > 0) {
     $carnumber = $row['carnumber'];
     $contact = $row['contact'];
 }
+
+$_SESSION['message'] =  "Are you sure of releasing parking space??";
+$_SESSION['alert-class'] = "alert alert-warning";
+
+//############################################
+//codes to release space
+if (isset($_POST['submit'])) {
+
+    if (count($errors) === 0) {
+        $bookSQL = "DELETE FROM booking WHERE id=:id;
+                    UPDATE parking_lot SET status='Active' WHERE name=:parking_lot";
+        $bookSQLStmt = $conn->prepare($bookSQL);
+        $bookSQLStmt->bindParam('s', $parking_lot, PDO::PARAM_STR);
+        $bookSQLStmt->bindParam(':id', $id, PDO::PARAM_INT);
+
+        //execute query
+        $is_executed = $bookSQLStmt->execute([
+            'parking_lot' => $parking_lot,
+            'id' => $id
+        ]);
+
+        if ($is_executed) {
+            $_SESSION['message'] =  $parking_lot . ' Released Successfully' . $indexnumber;
+            $_SESSION['alert-class'] = "alert alert-success";
+        } else {
+            $_SESSION['message'] =  "Booking Release Failed";
+            $_SESSION['alert-class'] = "alert alert-danger";
+        }
+    }
+}
+
+
