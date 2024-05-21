113e098ea554b052078b299492c5de5aac42c38d
Toka2104550 <125702984+Toka2104550@users.noreply.github.com>
2024-05-21 22:42:22 +0300

    c

--- student/view-parking-slot.php
+++ student/view-parking-slot.php
@@ -159,7 +159,7 @@ include('header.php');
 </div>
 </div>
 
-<!--Script JS-->
+
 <script src="js/jquery-3.4.1.min.js"></script>
 <script src="js/bootstrap.bundle.min.js"></script>
 <script src="js/scripts.js"></script>
